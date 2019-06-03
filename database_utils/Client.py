6e11fa35f8154d6a96a5b33974131f0924cadeca
Xian Zhang <xz278@cornell.edu>
2019-06-03 17:08:01 +0800

    Add create_table and insert_dataframe

--- database_utils/Client.py
+++ database_utils/Client.py
@@ -17,6 +17,7 @@ class Client:
             Name of connections.
         """
         self.name = name
+        self.database = name.split('_')[0]
         conf = helper.read_database_conf()
         if name not in conf:
             raise Exception("no database connection with name '{}'".format(name))
@@ -28,7 +29,11 @@ class Client:
         Performa query that read data ("select" statement)
         """
         t1 = time.time()
-        ret = postgres_helper.fetchall(config=self.conf, sql=query)
+        if self.database in ['redshift', 'postgres']:
+            ret = postgres_helper.fetchall(config=self.conf, sql=query)
+        else:
+            raise Exception("database not supported yet: '{}'"
+                            .fomat(self.database))
         t2 = time.time()
         t = t2 - t1
         print('Finished in {:.2f} seconds.'.format(t))
@@ -39,7 +44,52 @@ class Client:
         Perform write actions.
         """
         t1 = time.time()
-        postgres_helper.execute(conf=self.conf, query=query)
+        if self.database in ['redshift', 'postgres']:
+            postgres_helper.execute(conf=self.conf, query=query)
+        else:
+            raise Exception("database not supported yet: '{}'"
+                            .fomat(self.database))
+        t2 = time.time()
+        t = t2 - t1
+        print('Finished in {:.2f} seconds.'.format(t))
+        return
+
+
+    def create_table(self, table_info, table_name):
+        """
+        Create table
+        """
+        t1 = time.time()
+        if self.database in ['redshift', 'postgres']:
+            postgres_helper.create_table(
+                conf=self.conf,
+                table_info=table_info,
+                table_name=table_name
+            )
+        else:
+            raise Exception("database not supported yet: '{}'"
+                            .fomat(self.database))
+        t2 = time.time()
+        t = t2 - t1
+        print('Finished in {:.2f} seconds.'.format(t))
+        return
+
+
+    def insert_dataframe(self, table_info, table_name, data):
+        """
+        Create table
+        """
+        t1 = time.time()
+        if self.database in ['redshift', 'postgres']:
+            postgres_helper.insert_into_rdf(
+                conf=self.conf,
+                table_info=table_info,
+                table_name=table_name,
+                data=data
+            )
+        else:
+            raise Exception("database not supported yet: '{}'"
+                            .fomat(self.database))
         t2 = time.time()
         t = t2 - t1
         print('Finished in {:.2f} seconds.'.format(t))
--- database_utils/postgres_helper.py
+++ database_utils/postgres_helper.py
@@ -15,11 +15,11 @@ def fetchall(config, sql):
     Fetch data from PostgreSQL
 
     >>>redshift = {
-        'host': 'dwh.cgqfpdw0lgfw.cn-northwest-1.redshift.amazonaws.com.cn',
+        'host': '',
         'port': 5439,
         'dbname': 'test',
-        'user': 'mcc_dw_admin',
-        'password': 'mj3HcEwC5rV37UmaKqhc'
+        'user': 'admin',
+        'password': '123'
     }
     >>>df = postgres_fetchall(redshift=config, sql='select * from loan;')
 
@@ -68,3 +68,109 @@ def execute(conf, query):
     finally:
         if conn is not None:
             conn.close()
+
+
+def generate_create_query(table_info, table_name):
+    data_type = {
+        'Date': 'DATE',
+        'Int': 'INT8',
+        'Double': 'FLOAT8',
+        'String': 'VARCHAR(65535)'
+    }
+
+    create_template = """\
+CREATE TABLE {} (
+{}
+)"""
+    # comment
+    comment_string = "/*\nQuery created on: {}\n*/\n".format(
+        datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    )
+
+    # query
+    fields = ',\n'.join(
+        ['    {} {}'.format(a, data_type[b].replace('65535', c)) if b == 'String' and c.isdigit()
+         else '    {} {}'.format(a, data_type[b])
+         for a, b, c in table_info[['field', 'type', 'type_param']].values]
+    )
+    return comment_string + create_template.format(table_name, fields)
+
+
+
+def create_table(table_info, table_name, conf):
+    """
+    >>> create_table(table_info=table_info,
+                         table_name='test_table',
+                         conf=redshift_config)
+    Parameters:
+    -----------
+    table_info: pandas.DataFrame
+        Table schema, with columns ['field', 'type', 'type_param'].
+
+    table_name: str
+        Name of the table to be created.
+
+    conf: dict
+        Redshift connection configuration.
+    """
+    query = generate_create_query(table_info=table_info, table_name=table_name)
+    execute(conf=conf, query=query)
+    return
+
+
+def generate_insert_query(table_name, data, table_info):
+
+    insert_template = """
+INSERT INTO {} ({})
+VALUES
+{};
+"""
+    # comment
+    comment_string = "/*\nQuery created on: {}\n*/\n".format(
+        datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    )
+    data = data.copy()
+    for c in table_info.loc[table_info['type'] == 'String', 'field']:
+        data[c] = data[c].apply(lambda x: x if pd.isnull(x) else "'{}'".format(x))
+
+    for f, t in table_info.loc[table_info['type'] == 'Date', ['field', 'type_param']].values:
+        data[f] = data[f].apply(lambda x: x if pd.isnull(x) else "to_date('{}', '{}')".format(x, t))
+
+    for c in table_info.loc[table_info['type'].isin(['Double', 'Int']), 'field']:
+        data[c] = data[c].apply(lambda x: x if pd.isnull(x) else x.replace(',', '') if isinstance(x, str) else x)
+
+    data = data.fillna('NULL')
+
+    columns = ', '.join(data.columns.values)
+
+    values = ',\n'.join(['({})'.format(', '.join(x)) for x in data.values])
+
+    return comment_string + insert_template.format(table_name, columns, values)
+
+
+
+def insert_into_rdf(data, table_info, table_name, conf):
+    """
+    >>> insert_into_rdf(data=data,
+                        table_info=table_info,
+                        table_name='test_table',
+                        conf=redshift_config)
+    Parameters:
+    -----------
+    data: pandas.DataFrame
+        Data to insert.
+
+    table_info: pandas.DataFrame
+        Table schema, with columns ['field', 'type', 'type_param'].
+
+    table_name: str
+        Name of the table to be inserted into.
+
+    conf: dict
+        Redshift connection configuration.
+    """
+    query = generate_insert_query(table_name=table_name,
+                                  data=data,
+                                  table_info=table_info)
+    execute(conf=conf, query=query)
+    return
